0c37e2be20ae6048503d7392dbd3bca4535bd95e
Nicolas Seynaeve <nicolas.sey@gmail.com>
2021-01-18 15:21:32 +0100

    wip main modules transition

---
+++ modules/forum/src/Models/Post.php
@@ -0,0 +1,32 @@
+<?php
+namespace Modules\Forum\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\Personnages\Models\Personnage;
+
+class Post extends Model
+{
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    public function author()
+    {
+        return $this->belongsTo(Personnage::class, "author_id");
+    }
+
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($model) {
+            $model->topic->author->owner->follow($model);
+        });
+    }
+}
---
+++ modules/forum/src/Models/Topic.php
@@ -0,0 +1,59 @@
+<?php
+namespace Modules\Forum\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\Personnages\Models\Personnage;
+use Overtrue\LaravelFollow\Traits\CanBeFollowed;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Topic extends Model
+{
+    use SoftDeletes, HasSlug, CanBeFollowed;
+
+    protected $guarded = [];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    public function forum()
+    {
+        return $this->belongsTo(Forum::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(Personnage::class, "author_id");
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function lastPost()
+    {
+        return $this->posts()->latest("created_at");
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::deleted(function ($model) {
+            $model->posts()->delete();
+        });
+
+        static::created(function ($model) {
+            $model->author->owner->follow($model);
+        });
+    }
+}
