c8840a7c4305e4563984e1b001e7766d46ce7dc5
Nicolas Seynaeve <nicolas.sey@gmail.com>
2021-02-18 18:03:44 +0100

    compress migrations in Personnage module

--- modules/personnages/database/migrations/2018_11_27_205625_create_personnages.php
+++ modules/personnages/database/migrations/2018_11_27_205625_create_personnages.php
@@ -13,20 +13,29 @@ class CreatePersonnages extends \Illuminate\Database\Migrations\Migration
             $table->softDeletes();
 
             $table->unsignedMediumInteger("owner_id")->nullable();
+            $table->boolean("hide")->default(false);
+            $table->boolean("current")->default(false);
+
+            $table->string("name");
+            $table->string("slug");
+            $table->unsignedMediumInteger("age")->default(18);
+
+            $table->string("job")->nullable();
+            $table->string("title")->nullable();
 
             $table->boolean("active")->default(true);
             $table->boolean("isStaff")->default(false);
             $table->boolean("alive")->default(true);
-            $table->string("name");
-            $table->string("slug");
+
+            
             $table->text("bio")->nullable();
+            $table->text('physical')->nullable();
             $table->text("signature")->nullable();
             $table->text("affections")->nullable();
             $table->text("aversions")->nullable();
-            $table->string("job")->nullable();
-            $table->string("title")->nullable();
-            $table->boolean("hide")->default(false);
-            $table->boolean("current")->default(false);
+            $table->string("location")->nullable();
+
+            $table->boolean("hasForce")->default(false);
         });
     }
 
